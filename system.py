dbd363734df41dcc3b308f05cf6b4d348d73febe
shindeapurva1119 <shindeapurva066@gmail.com>
2025-04-08 18:51:56 +0530

    updated system.py

--- system.py
+++ system.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     response = client.run_instances(
         ImageId='ami-0614680123427b75e',
         InstanceType='t2.micro',
-        KeyName='aws',
+        KeyName='azure',
         MaxCount=1,
-        MinCount=1
+        MinCount=2
     )
